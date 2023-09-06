ba5f30f1fcf2312447b5eb15adc49637642ec346
Joyt <bigboom1122@gmail.com>
2023-09-06 14:36:50 +0300

    proxy change 4

--- hexletchat/src/index.js
+++ hexletchat/src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
-import init from './init';
+import init from './init.js';
 
 const app = async () => {
   const root = ReactDOM.createRoot(document.getElementById('chat'));
