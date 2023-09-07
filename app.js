8f67af5953be0b8efb0ba55d31b732955f8e0d86
Joyt <bigboom1122@gmail.com>
2023-09-07 14:14:13 +0300

    eslint fix 3

--- app.js
+++ app.js
@@ -20,13 +20,13 @@ module.exports = async function (fastify, opts) {
   // through your application
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts),
+    options: {...opts},
   });
 
   // This loads all plugins defined in routes
   // define your routes in one of these
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts),
+    options: {...opts},
   });
 };
